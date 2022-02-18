fd5b26036a6f3e257b49f37c1d83ab498ba52c91
Toredo <toredo89@mail.ru>
2022-02-18 23:19:48 +0500

    Выполнил дз-2

---
+++ src/lesson_2/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package lesson_2;
+
+public class MyArraySizeException extends Exception {
+    public java.lang.String i;
+    public java.lang.String j;
+    //    MyArraySizeException(String msg) {
+//        super(msg);
+//    }
+}
+
