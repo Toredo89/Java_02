dc6e484b1604303cef53272f4289ee5adea41d19
Toredo <toredo89@mail.ru>
2022-02-26 22:42:05 +0500

    Выполнил дз-2

--- src/lesson_2/Main.java
+++ src/lesson_2/Main.java
@@ -1,41 +1,69 @@
 package lesson_2;
 
 public class Main {
-    public static void main(String[] args) throws MyArraySizeException {
+    static String[][] arrayDataException = {{"1", "2", "3", "4"}, {"7", "6", "7", "8"}, {"9", "ш", "7", "8"}, {"5", "6", "7", "8"}};
 
-        String[][] arr = new String[][]{{"1", "2", "3", "4"}, {"8", "8", "8", "8"}, {"1", "2", "2", "2"}, {"2", "2", "2", "2"}};
+    static String[][] arraySizeException = {{"1", "2"}, {"1", "6",}, {"9", "6"}, {"5", "6"}};
+
+    static String[][] arrayWithNoError = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "6", "7", "8"}, {"5", "6", "7", "8"}};
+
+    static int sizeA = 4;
+    static int sizeB = 4;
+
+    public static void main(String[] args) {
+
         try {
-            int result = method(arr);
-            System.out.println(result);
-        } catch (MyArraySizeException e) {
-            System.out.println("Размер массива превышен!");
-        } catch (MyArrayDataException e) {
-            e.printStackTrace();
+            System.out.println("Сумма массива: " + getSumm(arrayWithNoError));
+        } catch (MyArraySizeException | MyArrayDataException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма массива: " + getSumm(arraySizeException));
+        } catch (MyArraySizeException | MyArrayDataException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма массива: " + getSumm(arrayDataException));
+        } catch (MyArraySizeException | MyArrayDataException ex){
+            System.out.println(ex.getMessage());
         }
     }
 
-    public static int method() throws MyArraySizeException, MyArrayDataException {
-        return method();
+    static void checkValue(String[][] array) throws MyArraySizeException{
+        if(array.length!=sizeA || array[0].length!=sizeB) throw new MyArraySizeException();
     }
 
-    public static int method(String[][] arr ) throws MyArraySizeException, MyArrayDataException {
-        int count = 0;
-        if (arr.length != 4) {
-            throw new MyArraySizeException();
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4) {
-                throw new MyArraySizeException();
-            }
-        for (int j = 0; j < arr.length; j++) {
-            try {
-                count = count + Integer.parseInt(arr[i][j]);
-            }
-            catch (NumberFormatException e) {
-                throw new MyArrayDataException(i, j);
+    static Integer getSumm(String[][] array) throws MyArrayDataException,MyArraySizeException{
+        checkValue(array);
+        Integer result = 0;
+        int i =0;
+        int j =0;
+        try {
+            for (; i < array.length; i++) {
+                j =0;
+                for (; j < array.length; j++) {
+                    result = result + Integer.parseInt(array[i][j]);
                 }
             }
+        } catch (NumberFormatException ex){
+            throw new  MyArrayDataException("Ошибка в позиции: " + i+","+j);
         }
-        return count;
+
+        return result;
+    }
+
+    public static class MyArraySizeException extends Exception {
+        public MyArraySizeException(){
+            super("Ошибка размера");
         }
     }
+
+    public static class MyArrayDataException extends Exception {
+        public MyArrayDataException(String message){
+            super(message);
+        }
+    }
+
+}
--- src/lesson_2/MyArraySizeException.java
+++ src/lesson_2/MyArraySizeException.java
@@ -3,8 +3,5 @@ package lesson_2;
 public class MyArraySizeException extends Exception {
     public java.lang.String i;
     public java.lang.String j;
-    //    MyArraySizeException(String msg) {
-//        super(msg);
-//    }
 }
 
