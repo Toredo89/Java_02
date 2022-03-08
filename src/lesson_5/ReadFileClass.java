d3b8da726acc83aac0a4f263afc4363c159d73e1
Toredo <toredo89@mail.ru>
2022-03-08 21:19:57 +0500

    Исправил дз-5

--- src/lesson_5/ReadFileClass.java
+++ src/lesson_5/ReadFileClass.java
@@ -1,9 +1,9 @@
 package lesson_5;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class ReadFileClass {
@@ -11,14 +11,47 @@ public class ReadFileClass {
     public static ArrayList<FIleObject> fIleObjectArrayList = new ArrayList<>();
     public static final String pathToFile = "src/lesson_5/Task.csv";
     public static final String title = "value1"+ ";" + "value2"
-            + ";" + "value3" + ";" + System.getProperty("line.separator");
-
+            + ";" + "value3" + ";"+ System.getProperty("line.separator");
 
 
     public static void main(String[] args) throws IOException {
-
+        createFileObjects();
+        writeStream();
 
         AppData appData = readToObject();
+    }
+
+    public static void createFileObjects(){
+        Random random = new Random();
+
+        for(int i=1;i<50;i++){
+            fIleObjectArrayList.add( new FIleObject(i, random.nextInt(50), random.nextInt(100)));
+        }
+    }
+
+    public static void writer() throws IOException {
+        try (FileWriter writer = new FileWriter(pathToFile);){
+            writer.write(title);
+            for(FIleObject fIleObject : fIleObjectArrayList) {
+                writer.write(fIleObject.getValue() + ";" + fIleObject.getValueFrom()
+                        + ";" + fIleObject.getValueFrom() + ";"+ System.getProperty("line.separator"));
+            }
+        }
+    }
+
+    public static void writeStream() throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(pathToFile)){
+            for(byte b :title.getBytes(StandardCharsets.UTF_8)){
+                fileOutputStream.write(b);
+            }
+            for(FIleObject fIleObject : fIleObjectArrayList){
+                String raw = fIleObject.getValue() + ";" + fIleObject.getValueFrom()
+                        + ";" + fIleObject.getValueTo() + ";"+ System.getProperty("line.separator");
+                for(byte b :raw.getBytes(StandardCharsets.UTF_8)){
+                    fileOutputStream.write(b);
+                }
+            }
+        }
     }
 
     public static AppData readToObject() throws IOException {
@@ -26,14 +59,17 @@ public class ReadFileClass {
         try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
             String line = br.readLine();
             appData.setHeader( line.split(";"));
-            while (null != (line = br.readLine())) {
+            while ((line = br.readLine()) != null) {
                 String[] values = line.split(";");
                 System.out.println(title);
                 System.out.println(line);
             }
-        }
+        };
 
         return appData;
     }
 }
+
+
+
 
